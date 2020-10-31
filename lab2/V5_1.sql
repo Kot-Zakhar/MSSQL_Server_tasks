cfa7b385de3c1a4d596ea2f3e9410b2dc60e0caa
Kot-Zakhar <kot_zakhar@mail.ru>
2020-10-31 21:08:58 +0300

    Fixing mistakes in 2 lab

--- lab2/V5_1.sql
+++ lab2/V5_1.sql
@@ -9,7 +9,8 @@ from HumanResources.Employee
 inner join HumanResources.EmployeeDepartmentHistory
 	on Employee.BusinessEntityID = EmployeeDepartmentHistory.BusinessEntityID
 inner join HumanResources.Shift
-	on EmployeeDepartmentHistory.ShiftID = Shift.ShiftID;
+	on EmployeeDepartmentHistory.ShiftID = Shift.ShiftID
+where EmployeeDepartmentHistory.EndDate is null;
 go
 
 /*
@@ -30,47 +31,24 @@ go
 	В рамках каждого отдела разбейте все ставки на группы, таким образом,
 	чтобы ставки с одинаковыми значениями входили в состав одной группы.
 */
-select Name, DepHist.BusinessEntityID, Rate, MaxInDepartment,
-	dense_rank() over(partition by Dep.DepartmentID order by Rate) RateGroup
-from HumanResources.EmployeeDepartmentHistory as DepHist
-inner join HumanResources.Department as Dep
-	on DepHist.DepartmentID = Dep.DepartmentID
-inner join HumanResources.EmployeePayHistory as PayHist
-	on DepHist.BusinessEntityID = PayHist.BusinessEntityID
-inner join (/*Calculating MaxInDepartment value for each Dep*/
-	select DepartmentID, MAX(Rate) as MaxInDepartment
+with data as (
+	select Dep.DepartmentID, PayHist.BusinessEntityID, Name, Rate, DepHist.EndDate
 	from HumanResources.EmployeeDepartmentHistory as DepHist
-	inner join HumanResources.EmployeePayHistory as PayHist
-		on DepHist.BusinessEntityID = PayHist.BusinessEntityID
-	where DepHist.EndDate is null
-	group by DepartmentID
-) MaxInDeps
-on Dep.DepartmentID = MaxInDeps.DepartmentID
-where DepHist.EndDate is null
-order by Name, Rate;
-go
-
-/*
-P.S.:
-How can i reuse first two inner joins inside of the subquery?
-smth like the following:
-
-select ...
-
-from (
-	select * from HumanResources.EmployeeDepartmentHistory as DepHist
 	inner join HumanResources.Department as Dep
 		on DepHist.DepartmentID = Dep.DepartmentID
 	inner join HumanResources.EmployeePayHistory as PayHist
 		on DepHist.BusinessEntityID = PayHist.BusinessEntityID
-) as Rates
-
-inner join (
+)
+select Name, BusinessEntityID, Rate, MaxInDepartment,
+	dense_rank() over(partition by data.DepartmentID order by Rate) RateGroup
+from data
+inner join (/*Calculating MaxInDepartment value for each Dep*/
 	select DepartmentID, MAX(Rate) as MaxInDepartment
-	from Rates
+	from data
+	where EndDate is null
 	group by DepartmentID
 ) MaxInDeps
-on Rates.DepartmentID = MaxInDeps.DepartmentID
-...;
-
-*/
+on data.DepartmentID = MaxInDeps.DepartmentID
+where EndDate is null
+order by Name, Rate;
+go
